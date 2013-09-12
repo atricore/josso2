cce2432d30a42b0df421019d2157e21c4e6f494a
Sebastian Gonzalez Oyuela <sgonzalez@localhost.localdomain>
2013-09-12 11:05:15 -0400

    * NEW : Updated archetype to latest UI project

---
+++ distributions/josso-ce/src/main/distribution/unix-text/bin/tools/create-ui-project.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+PROGNAME=`basename $0`
+
+JOSSO2_VERSION=2.4.0-SNAPSHOT
+JOSSO2_REPO=http://repository.josso.org/m2-release-repository,http://repository.josso.org/m2-snapshot-repository
+
+warn() {
+    echo "${PROGNAME}: $*"
+}
+
+die () {
+    warn "$*"
+    exit 1
+}
+
+
+printUsage () {
+    die "Usage <group-id> <ui-id>"
+}
+
+init() {
+
+   if [ "x$GROUP_ID" = "x" ]; then
+        GROUP_ID=$1
+   fi
+
+   if [ "x$UI_ID" = "x" ]; then
+        UI_ID=$2
+   fi
+
+   if [ "x$GROUP_ID" = "x" ]; then
+        printUsage
+   fi
+
+   if [ "x$UI_ID" = "x" ]; then
+        printUsage
+   fi
+
+
+   if [ "x$VARIANT" = "x" ]; then
+        VARIANT=$UI_ID
+   fi
+
+   if [ "x$PACKAGE" = "x" ]; then
+        PACKAGE=$GROUP_ID
+   fi
+
+   if [ "x$ARTIFACT_ID" = "x" ]; then
+        ARTIFACT_ID=$UI_ID
+   fi
+
+   VARIANT_SUFIX=_$VARIANT
+
+}
+
+run() {
+    mvn -B  archetype:generate -Dmaven.repo.remote=${JOSSO2_REPO} -DarchetypeGroupId=org.atricore.josso.archetypes -DarchetypeArtifactId=josso2-ui-archetype -DarchetypeVersion=${JOSSO2_VERSION} -DgroupId=${GROUP_ID} -DartifactId=${ARTIFACT_ID} -Dpackage=${PACAKGE} -DuiId=${UI_ID} -Dvariatn=${VARIANT} -DvariantSufix=${VARIANT_SUFIX}
+}
+
+main() {
+    init "$@"
+    run "$@"
+}
+
+main "$@"
---
+++ distributions/josso-ce/src/main/distribution/windows-text/bin/tools/create-ui-project.bat
@@ -0,0 +1,55 @@
+@echo off
+rem
+rem
+rem
+
+if not "%ECHO%" == "" echo %ECHO%
+
+setlocal
+set DIRNAME=%~dp0%
+set PROGNAME=%~nx0%
+set ARGS=%*
+set JOSSO2_VERSION=2.4.0-SNAPSHOT
+set JOSSO2_REPO=http://repository.josso.org/m2-release-repository,http://repository.josso.org/m2-snapshot-repository
+
+goto :BEGIN
+
+:warn
+    echo %PROGNAME%: %*
+goto :EOF
+
+:BEGIN
+
+if "%GROUP_ID%" == "" set GROUP_ID=%1
+
+if "%UI_ID%" == "" set UI_ID=%2
+
+if not "%GROUP_ID%" == "" goto :CHECK_GROUP_END
+    call :warn Usage group-id ui-id
+    goto END
+:CHECK_GROUP_END
+
+if not "%UI_ID%" == "" goto :CHECK_UI_END
+    call :warn Usage group-id ui-id
+    goto END
+:CHECK_UI_END
+
+if "%VARIANT%" == "" set VARIANT=%UI_ID%
+
+if "%PACKAGE%" == "" set PACKAGE=%GROUP_ID%
+
+if "%ARTIFACT_ID%" == "" set ARTIFACT_ID=%UI_ID%
+
+set VARIANT_SUFIX = _%VARIANT%
+
+:RUN
+set ARGS=%1 %2 %3 %4 %5 %6 %7 %8
+mvn -B  archetype:generate -Dmaven.repo.remote="%JOSSO2_REPO%" -DarchetypeGroupId="org.atricore.josso.archetypes" -DarchetypeArtifactId="josso2-ui-archetype" -DarchetypeVersion="%JOSSO2_VERSION%" -DgroupId="%GROUP_ID%" -DartifactId="%ARTIFACT_ID%" -Dpackage="%PACAKGE%" -DuiId="%UI_ID%" -Dvariatn="%VARIANT%" -DvariantSufix="%VARIANT_SUFIX%"
+
+:END
+
+endlocal
+
+if not "%PAUSE%" == "" pause
+
+:END_NO_PAUSE
